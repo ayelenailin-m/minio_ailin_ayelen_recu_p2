edf53bc9f01454de9fca0a441858c1212e549e52
ayelenailin-m <pukyailin@gmail.com>
2024-07-04 15:37:31 -0300

    ruta y controlador para crear un nuevo usuario

--- src/app.js
+++ src/app.js
@@ -20,6 +20,31 @@ app.get('/students/:id', (req, res) => {
     }
 });
 
+const validateEstudent = (req, res, next) => {
+    const { fullName, age, curse } = req.body;
+
+    if (typeof fullName !== 'string' || fullName === '' || !Number.isInteger(age) || age < 11 || age > 20 || typeof curse !== 'string' || curse === '') {
+        return res.status(400).json({
+            mensaje: "Datos inválidos. 'fullName' y 'curse' deben ser una cadena de texto no vacía, 'age' debe ser un número entero entre 11 y 20."
+        });
+    }
+    req.body.fullName = fullName.trim();
+    next();
+};
+app.post('/students', validateEstudent, (req, res) => {
+    const id = Generator();
+    const { fullName, age, curse } = req.body;
+
+    const exists = students.find((e) => e.fullName === fullName);
+    if (exists) {
+        return res.status(409).json({ mensaje: 'Este estudiante ya existe en la lista' });
+    }
+
+    const newStudent = { id, fullName, age, curse };
+    students.push(newStudent);
+    res.status(201).json({ mensaje: 'Estudiante agregado con éxito', Estudiante: newStudent });
+});
+
 
 app.listen(4321, () => {
     console.log('Servidor corriendo en el puerto 4321');
